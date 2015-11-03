9d8347a4b236ff6a9f5c78e4988aab4216d1333c
Pablo <pablo@bluefinapps.com>
2015-11-03 11:40:40 -0200

    improve code facebookSDK

--- SocialIntegration/ProfileViewController.m
+++ SocialIntegration/ProfileViewController.m
@@ -29,9 +29,6 @@
     
     [self.view addSubview:self.logoutButton];
     
-//    NSLog(@"%@", [FBSDKAccessToken currentAccessToken]);
-//    NSString *aux = [NSString stringWithFormat:@"https://graph.facebook.com/me?access_token=%@", [FBSDKAccessToken currentAccessToken]];
-
 
     [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:@{@"fields" : @"email, name, picture.type(large), birthday, friends"}]
      startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
@@ -52,46 +49,11 @@
              
          }
      }];
-    
-
-    //[self loadProfile];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-//-(void)loadProfile{
-//    self.userName.text = [self showName];
-//}
-
-//-(NSString *)showName{
-//    NSString *name;
-//
-//    FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc]
-//                                  initWithGraphPath:@"/{user-id}"
-//                                  parameters:nil
-//                                  HTTPMethod:@"GET"];
-//    [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection,
-//                                          id result,
-//                                          NSError *error) {
-//        // Handle the result
-//        NSLog(@"%@",result);
-//    }];
-//
-//
-//    return name;
-//}
-
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    if ([[segue identifier] isEqualToString:@"showPost"]) {
-
-        // Get destination view
-        PostViewController *vc = [segue destinationViewController];
-
-
-    }
 }
 
 #pragma mark - delegate methods
@@ -101,12 +63,9 @@
     //[loginManager logOut];
     //[FBSDKProfile setCurrentProfile:nil];
     
-    
     [self performSegueWithIdentifier:@"showLogin" sender:loginButton];
 }
 
--(void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error{
-    
-}
+-(void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error{}
 
 @end
--- SocialIntegration/Social Framework/TwitterSocialFViewController.h
+++ SocialIntegration/Social Framework/TwitterSocialFViewController.h
@@ -8,6 +8,6 @@
 
 #import "ViewController.h"
 
-@interface TwitterSocialFViewController : ViewController
+@interface TwitterSocialFViewController : UIViewController
 
 @end
